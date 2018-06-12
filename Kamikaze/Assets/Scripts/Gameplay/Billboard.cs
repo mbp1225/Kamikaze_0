6f78735d39428c4bbad82bd6fe5fa80aa3a119fe
mbp1225 <matheus698@hotmail.com>
2018-06-12 10:24:58 -0300

    Sprite Fixed

---
+++ Kamikaze/Assets/Scripts/Gameplay/Billboard.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+	void Update ()
+	{
+		//transform.LookAt(Camera.main.transform.position);
+		//transform.localEulerAngles = new Vector3(-30, transform.localEulerAngles.y, 0);
+		transform.rotation = Quaternion.LookRotation(-Camera.main.transform.forward);
+	}
+}
